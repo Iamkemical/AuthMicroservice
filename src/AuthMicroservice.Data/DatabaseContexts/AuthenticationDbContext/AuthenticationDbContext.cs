616dba3ca36dcbd1700d88858d9da22a643e4001
Gabriel Isaac <iamkemical1@gmail.com>
2020-07-13 20:21:33 +0100

    Added AuthenticationDbContext

--- src/AuthMicroservice.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/AuthMicroservice.Data/DatabaseContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,7 +1,15 @@
+using AuthMicroservice.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace AuthMicroservice.Data.DatabaseContexts.AuthenticationDbContext
 {
-    public class AuthenticationDbContext
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
-        
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+        :base(options)
+        {
+            
+        }
     }
 }
---
+++ src/AuthMicroservice.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthMicroservice.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
