0b6f76db4d89555777d822cb5f7f295bfc8d9e07
Gabriel Isaac <iamkemical1@gmail.com>
2020-07-19 18:58:57 +0100

    Added API and Data project

---
+++ src/AuthMicroservice.Data/DatabaseContexts/UserDbContext.cs
@@ -0,0 +1,15 @@
+using AuthMicroservice.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthMicroservice.Data.DatabaseContexts
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+        :base(options)
+        {
+            
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ src/AuthMicroservice.Data/Entities/User.cs
@@ -0,0 +1,13 @@
+namespace AuthMicroservice.Data.Entities
+{
+    public class User
+    {
+        public string UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
